ed6b7678e630fd7bec5e635a948a4133019c0a2c
John Eargle <john.eargle@lab7.io>
2020-03-14 15:53:01 -0500

    #1 switch over to using __main__

--- m4a2mp3.py
+++ m4a2mp3.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 
-from os import listdir
+import os
 from subprocess import call
 
-files = listdir()
 
-for file in listdir():
-    # print(file + ' -- ' + '.'.join(file.split('.')[0:-1]) + '.mp3')
-    if file.split('.')[-1] == 'm4a':
-        # print('YAY')
-        prefix = '.'.join(file.split('.')[0:-1])
-        ffmpeg_command = 'ffmpeg "{}.mp3" -i "{}.m4a" -codec:a libmp3lame -qscale:a 1'.format(prefix, prefix)
-        print(ffmpeg_command)
-        call(ffmpeg_command, shell=True)
+def m4a2mp3(path):
+    files = os.listdir(path)
+
+    for file in files:
+        if file.split('.')[-1] == 'm4a':
+            prefix = os.path.join(path, '.'.join(file.split('.')[0:-1]))
+            ffmpeg_command = 'ffmpeg "{}.mp3" -i "{}.m4a" -codec:a libmp3lame -qscale:a 1'.format(prefix, prefix)
+            print(ffmpeg_command)
+            call(ffmpeg_command, shell=True)
+
+
+
+if __name__=='__main__':
+    m4a2mp3('.')
