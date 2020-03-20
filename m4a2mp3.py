f798e569057ac8232496bf0bc46cda3dd5b2ecf8
John Eargle <jeargle@gmail.com>
2020-03-20 18:48:06 -0500

    #5 force flag for overwriting mp3 files

--- m4a2mp3.py
+++ m4a2mp3.py
@@ -5,15 +5,20 @@ import os
 from subprocess import call
 
 
-def m4a2mp3(path):
+def m4a2mp3(path, force=False):
     files = os.listdir(path)
 
     for file in files:
         if file.split('.')[-1] == 'm4a':
             prefix = os.path.join(path, '.'.join(file.split('.')[0:-1]))
-            ffmpeg_command = 'ffmpeg "{}.mp3" -i "{}.m4a" -codec:a libmp3lame -qscale:a 1'.format(prefix, prefix)
-            print(ffmpeg_command)
-            call(ffmpeg_command, shell=True)
+            outfile_name = prefix + ".mp3"
+
+            # Do not overwrite unless force is True
+            if (not os.path.isfile(outfile_name)) or force:
+                infile_name = prefix + ".m4a"
+                ffmpeg_command = 'ffmpeg -y "{}" -i "{}" -codec:a libmp3lame -qscale:a 1'.format(outfile_name, infile_name)
+                print(ffmpeg_command)
+                call(ffmpeg_command, shell=True)
 
 
 if __name__=='__main__':
@@ -25,6 +30,12 @@ if __name__=='__main__':
         help='convert files throughout the directory tree'
     )
     parser.add_argument(
+        '-f',
+        '--force',
+        action='store_true',
+        help='force overwrite of mp3 file(s)'
+    )
+    parser.add_argument(
         'path',
         type=str,
         help='path to the directory that contains the m4a files'
@@ -34,6 +45,6 @@ if __name__=='__main__':
     if args.recursive:
         for path, _, files in os.walk(args.path):
             if len(files) > 0:
-                m4a2mp3(path)
+                m4a2mp3(path, args.force)
     else:
-        m4a2mp3(args.path)
+        m4a2mp3(args.path, args.force)
